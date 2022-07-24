655b1a213015f4257d7e7fd2a39bd509f73dcae0
hoskyfull <norberto.cardenales@gmail.com>
2022-07-23 18:38:30 -0600

    added api key to env

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { ChannelList, Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+require('dotenv').config()
+
+const api_key = process.env.API_KEY
+const client = StreamChat.getInstance(api_key)
 
 const App = () => {
   return (
-    <div>
-        <h1>Chitchat</h1>
+    <div className='app__wrapper'>
+        <Chat>
+            <ChannelListContainer />
+            <ChannelContainer />
+        </Chat>
         </div>
   )
 }

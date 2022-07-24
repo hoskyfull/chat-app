e176644aafee35444134614460896cb451b89d93
hoskyfull <norberto.cardenales@gmail.com>
2022-07-23 23:52:23 -0600

    render basic h1 ask checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { StreamChat } from 'stream-chat'
 import { ChannelList, Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
-require('dotenv').config()
+import { ChannelContainer, ChannelListContainer } from './components'
+
+
 
 const api_key = process.env.API_KEY
 const client = StreamChat.getInstance(api_key)

596ed9c294c735ff10a0d00fbc13013c7ade8a38
hoskyfull <norberto.cardenales@gmail.com>
2022-07-24 13:24:22 -0600

    created logout button and added logos

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,15 +3,20 @@ import { ChannelList, useChatContext  } from 'stream-chat-react';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import Quaint from '../assets/q.png'
+import LogOutIcon from '../assets/logout.png'
 
 const SideBar = () => (
   <div className = "channel-list__sidebar ">
     <div className='channel-list__sidebar__icon1'>
       <div className='icon__inner'>
-        <img src={Quaint} alt='Chat' width="30" />
+        <img src={Quaint} alt='Quaint' width="30" />
       </div>
-
-    </div>
+    </div> 
+    <div className='channel-list__sidebar__icon2'>
+      <div className='icon__inner'>
+        <img src={LogOutIcon } alt='Logout' width="30" />
+      </div>
+    </div> 
 
   </div>
 )

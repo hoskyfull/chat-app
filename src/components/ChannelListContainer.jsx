a44f498b0298ff7ed04d73ad244b7252d3a890de
hoskyfull <norberto.cardenales@gmail.com>
2022-07-24 09:42:45 -0600

    working on channellistcontainer sidebar

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,6 +3,17 @@ import { ChannelList, useChatContext  } from 'stream-chat-react';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+const SideBar = () => (
+  <div className = "channel-list__sidebar ">
+    <div className='channel-list__sidebar__icon1'>
+      <div className='icon__inner'>
+        <img src={ChatIcon} alt='Chat' width="30" />
+      </div>
+
+    </div>
+
+  </div>
+)
 
 const ChannelListContainer = () => {
   return (

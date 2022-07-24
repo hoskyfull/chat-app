9748de7b8f33da94341cbe8e3b92ea212531035a
hoskyfull <norberto.cardenales@gmail.com>
2022-07-24 13:12:34 -0600

    added some preset styles

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { ChannelList, useChatContext  } from 'stream-chat-react';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
+import Quaint from '../assets/q.png'
 
 const SideBar = () => (
   <div className = "channel-list__sidebar ">
     <div className='channel-list__sidebar__icon1'>
       <div className='icon__inner'>
-        <img src={ChatIcon} alt='Chat' width="30" />
+        <img src={Quaint} alt='Chat' width="30" />
       </div>
 
     </div>

e50fd7abf2e5c657ab2187563d5269f7ed6ff45e
hoskyfull <norberto.cardenales@gmail.com>
2022-07-25 11:23:28 -0600

    added loading condition for the channel list

--- src/components/TeamChannelList.jsx
+++ src/components/TeamChannelList.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import { AddChannel } from "../assets";
 
 const TeamChannelList = ({ children, error = false, loading, type }) => {
-  if(error) {
-    return type === 'team' ? (
-
-    ) : null
+  if (error) {
+    return type === "team" ? (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message">
+          Connection error, please wait a moment and try again.
+        </p>
+      </div>
+    ) : null;
   }
+
+  if (loading) {
+    return (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message">
+          Connection error, please wait a moment and try again.
+        </p>
+      </div>
+    );
+  }
+
   return;
   <div>TeamChannelList</div>;
 };

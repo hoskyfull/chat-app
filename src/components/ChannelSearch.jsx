47823b389b848cf10d55d8303b9737dd4089be8c
hoskyfull <norberto.cardenales@gmail.com>
2022-07-25 08:38:38 -0600

    added fucntionality to the search input

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,16 +1,28 @@
+import { getSuggestedQuery } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
 import { SearchIcon } from '../assets'
 
 // working on the search bar for the page
 const ChannelSearch = () => {
-  const [query, seQuery] = useState('')
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const onSearch = (event) => {
-    event.preventDefault();
+  const getChannels = async ( text ) => {
+    try{
+      //TODO: fetch channels
+    }catch (error) {
+      setQuery('')
+    }
   }
 
-
+//logic that makes search fucnitonality work
+  const onSearch = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    getSuggestedQuery(event.target.value)
+    getChannels(event.target.value)
+  };
 
   return (
     <div className="channel-search__container">
@@ -18,12 +30,15 @@ const ChannelSearch = () => {
         <div className="channel-search__input__icon">
           <SearchIcon />
         </div>
-
-
+        <input
+          className="channel-search__input__text"
+          placeholder="Search"
+          type={query}
+          onChange={onSearch}
+        />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default ChannelSearch

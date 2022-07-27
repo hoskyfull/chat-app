246c58ede3b6d7007c862815148fc1609c8f6a62
hoskyfull <norberto.cardenales@gmail.com>
2022-07-27 09:19:52 -0600

    finshed temchannelpreview component

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -14,8 +14,31 @@ const TeamChannelPreview = ({ channel, type }) => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id != client.userID
     );
+
+    return (
+      <div className="channel-preview__item single">
+        <Avatar
+          image={members[0]?.user?.image}
+          name={members[0]?.user?.fullName}
+          size={24}
+        />
+        <p>{members[0]?.user?.fullName}</p>
+      </div>
+    );
   };
-  return <div>TeamChannelPreview</div>;
+  return;
+  <div
+    className={
+      channel?.id === activateChannel?.id
+        ? "channel-preview__wrapper__selected"
+        : "channel-preview__wrapper"
+    }
+    onClick={() => {
+      console.log(channel);
+    }}
+  >
+    {type === "team" ? <ChannelPreview /> : <ChannelPreview />}
+  </div>;
 };
 
 export default TeamChannelPreview;

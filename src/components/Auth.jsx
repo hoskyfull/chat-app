8faf7e47b21c4a5ed6cd8f290079674a053f2ef7
hoskyfull <norberto.cardenales@gmail.com>
2022-07-27 12:19:35 -0600

    working on auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,21 @@
-import React, { UseState } from "react";
+import React, { useState } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
+import signinImage from "../assets/signup.jpeg";
 
 const Auth = () => {
-    return <div>Auth</div>;
+    const [isSignup, setIsSignup] =
+        useState(false);
+    return (
+        <div className="auth__from-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{}</p>
+                </div>
+            </div>
+            Auth
+        </div>
+    );
 };
 
 export default Auth;

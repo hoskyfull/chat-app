2dc1f0e3d7080067d93ef37d0d318ccb1eace255
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 12:44:20 -0600

    finished signup function in the backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,6 +14,16 @@ const signup = async (req, res) => {
         const userId = crypto.randomBytes(16).toString("hex");
         const serverClient = connect(api_key, api_secret, app_id);
         const hashedPassword = await bcrypt.hash(password, 10);
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({
+            token,
+            fullName,
+            username,
+            userId,
+            hashedPassword,
+            phoneNumber,
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error });

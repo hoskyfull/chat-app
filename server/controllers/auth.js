e4a9c36759952b5e854531a8c4c17daf3fffde46
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 11:49:52 -0600

    added cath error for login route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,12 @@ const signup = () => {
         resizeBy.status(500).json({ message: error });
     }
 };
-const login = () => {};
+const login = () => {
+    try {
+    } catch (error) {
+        console.log(error);
+        resizeBy.status(500).json({ message: error });
+    }
+};
 
 module.exports = { signup, login };

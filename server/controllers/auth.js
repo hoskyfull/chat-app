32a3989f627baf5c355dd41762fc766237056bf5
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 12:03:49 -0600

    created variables for api key, secret and id

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,10 +3,15 @@ const bcrypt = require("bcrypt");
 const StreamChat = require("stream-chat");
 const crypto = require("crypto");
 
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const api_id = process.env.STREAM_API_ID;
+
 const signup = (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
-        const userId = crypto;
+        const userId = crypto.randomBytes(16).toString("hex");
+        const serverClient = connect(api_key, api_secret, app_id);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error });

8029e307a6ad36bf97877c9a823b7d8bd0549f0f
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 11:21:32 -0600

    made logic, backend officially running

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

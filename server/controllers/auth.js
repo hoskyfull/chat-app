7040f570d090108575d84dc4880fa44ffaa264ee
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 11:58:39 -0600

    working on ssignup logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,22 @@
-const signup = () => {
+const { connect } = require("getstream");
+const bcrypt = require("bcrypt");
+const StreamChat = require("stream-chat");
+const crypto = require("crypto");
+
+const signup = (req, res) => {
     try {
+        const { fullName, username, password, phoneNumber } = req.body;
+        const userId = crypto;
     } catch (error) {
         console.log(error);
-        resizeBy.status(500).json({ message: error });
+        res.status(500).json({ message: error });
     }
 };
-const login = () => {
+const login = (req, res) => {
     try {
     } catch (error) {
         console.log(error);
-        resizeBy.status(500).json({ message: error });
+        res.status(500).json({ message: error });
     }
 };
 

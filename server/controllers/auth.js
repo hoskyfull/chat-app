edd791017dd934da42847d04a145bd2bcbdd1b00
hoskyfull <norberto.cardenales@gmail.com>
2022-07-28 12:39:19 -0600

    created bycrupt variable

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,11 +8,12 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const api_id = process.env.STREAM_API_ID;
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
         const userId = crypto.randomBytes(16).toString("hex");
         const serverClient = connect(api_key, api_secret, app_id);
+        const hashedPassword = await bcrypt.hash(password, 10);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error });

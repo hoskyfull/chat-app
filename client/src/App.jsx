f2487970fc4e803a5de087df8852e9b4bc799676
hoskyfull <norberto.cardenales@gmail.com>
2022-07-29 11:37:09 -0600

    auth token logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { StreamChat } from "stream-chat";
-import { ChannelList, Chat} from "stream-chat-react";
+import { ChannelList, Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-import { ChannelContainer, ChannelListContainer, Auth} from "./components";
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 import "./App.css";
 
+const cookies = new Cookies();
+
 const api_key = process.env.API_KEY;
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(api_key);
 
-const authToken = false;
+if (authToken) {
+    client.connectUser({
+        id: cookies.get("userId"),
+        token: cookies.get("token"),
+        username: cookies.get("username"),
+        fullName: cookies.get("fullName"),
+        phoneNumber: cookies.get("phoneNumber"),
+        avatarURL: cookies.get("avatarURL"),
+        hashedPassword: cookies.get("hashedPassword"),
+    });
+}
 
 const App = () => {
     if (!authToken) return <Auth />;
 
     return (
         <div className="app__wrapper">
-            <Chat
-                client={client}
-                theme="team light"
-            >
+            <Chat client={client} theme="team light">
                 <ChannelListContainer />
                 <ChannelContainer />
             </Chat>

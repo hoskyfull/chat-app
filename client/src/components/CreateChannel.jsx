3349a034bcffc8f6353381d5b4be7b97582c96a6
hoskyfull <norberto.cardenales@gmail.com>
2022-07-31 14:43:51 -0600

    started working on the create channel logic

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
+
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
 
 const CreateChannel = () => {
     return <div>CreateChannel</div>;

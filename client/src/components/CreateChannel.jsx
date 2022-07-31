5eea32992ee76c64e8abc5067b4a3c1c9b3ea14c
hoskyfull <norberto.cardenales@gmail.com>
2022-07-31 14:45:28 -0600

    creating channel name input logic

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -4,6 +4,14 @@ import { useChatContext } from "stream-chat-react";
 import { UserList } from "./";
 import { CloseCreateChannel } from "../assets";
 
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+    const handleChange = (event) => {
+        event.preventDefault();
+
+        setChannelName(event.target.value);
+    }
+
 const CreateChannel = () => {
     return <div>CreateChannel</div>;
 };

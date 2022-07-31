9e78392656fe57dc767dfe5a446c336c2b128906
hoskyfull <norberto.cardenales@gmail.com>
2022-07-31 08:39:27 -0600

    working on channel container logic

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -3,6 +3,19 @@ import { Channel, useChatContext } from "stream-chat-react";
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
 
 const ChannelContainer = () => {
+    const { channel } = useChatContext();
+
+    if (isCreating) {
+        return (
+            
+        )
+    }
+    if (isEditing) {
+        return (
+
+        )
+    }
+
     return <div>ChannelContainer</div>;
 };
 

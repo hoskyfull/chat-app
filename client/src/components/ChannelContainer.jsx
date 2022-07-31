6e14dab6ae473a31ce312f519538f8bff88e3731
hoskyfull <norberto.cardenales@gmail.com>
2022-07-31 08:53:14 -0600

    finished editing logic

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -29,6 +29,17 @@ const ChannelContainer = ({
         );
     }
 
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">
+                This is the beginning of your chat history
+            </p>
+            <p className="channel-empty__second">
+                Send messages, attachments, links emojis and more!
+            </p>
+        </div>
+    );
+
     return <div>ChannelContainer</div>;
 };
 
